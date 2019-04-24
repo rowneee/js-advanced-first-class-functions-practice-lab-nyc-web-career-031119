7faf12d9235942ebc18770ea5d8e40900769fc8a
rowneee <roni.shabo@gmail.com>
2019-04-24 09:41:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const logDriverNames = function(drivers) {
+  drivers.forEach(function (driver) {
+  console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers,hometown) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+};
+
+const driversByRevenue = function(drivers) {
+  return drivers.slice().sort(function(driver1, driver2) {
+    return driver1.revenue - driver2.revenue;
+  });
+};
+
+const driversByName = function(drivers) {
+  return drivers.slice().sort(function(driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
+  });
+};
+
+const totalRevenue = function(drivers) {
+    return drivers.reduce(function (sum, driver) {
+      return driver.revenue + sum;
+  }, 0);
+};
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
